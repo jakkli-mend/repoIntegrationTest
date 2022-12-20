b812a66affdeb4b52af734fbf108fc26e4be6811
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2022-12-20 10:04:17 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -13,22 +13,4 @@ class Example {
 
     File(getParameter("inpu11t1-va1lue22333111"));
 
-    File(getParameter("inpu11t1-va1lue223331111111"));
-
-    File(getParameter("inpu11t1-va1lue22333111111111111"));
-
-    File(getParameter("inpu11t1-va1lue223331111111111111222222"));
-
-    File(getParameter("inpu11t1-va1lue223331111111111111222222344442"));
-
-    File(getParameter("inpu11t1-va1lue2233311111111111112222224"));
-
-    File(getParameter("inpu11t1-va1lue223331166611333777111"));
-
-    public static void main(String[] args) {
-        Trace t = new Trace();
-        t.foo1("input1");
-        //comment 33
-    }
-
-}//comment
+}

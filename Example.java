0116efc8c65b89f32facd350c5d14a8493a77bb0
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2022-12-20 09:45:05 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -2,33 +2,6 @@ import folder1.folder2.Trace;
 
 class Example {
     File(getParameter("input1-value"));
-
     File(getParameter("input1-va1lue"));
 
-    File(getParameter("input1-va1lue22"));
-
-    File(getParameter("inpu11t1-va1lue22"));
-
-    File(getParameter("inpu11t1-va1lue22333"));
-
-    File(getParameter("inpu11t1-va1lue22333111"));
-
-    File(getParameter("inpu11t1-va1lue223331111111"));
-
-    File(getParameter("inpu11t1-va1lue22333111111111111"));
-
-    File(getParameter("inpu11t1-va1lue223331111111111111222222"));
-
-    File(getParameter("inpu11t1-va1lue223331111111111111222222344442"));
-
-    File(getParameter("inpu11t1-va1lue2233311111111111112222224"));
-
-    File(getParameter("inpu11t1-va1lue223331166611333777111"));
-
-    public static void main(String[] args) {
-        Trace t = new Trace();
-        t.foo1("input1");
-        //comment 33
-    }
-
-}//comment
+}

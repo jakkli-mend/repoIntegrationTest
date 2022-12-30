d0248c84997f76287699f9746d239bf181027da1
Jakub Kliwison <jakub.kliwison@mend.io>
2022-12-30 18:02:38 +0100

    Update

--- Example.java
+++ Example.java
@@ -3,8 +3,7 @@ import folder1.folder2.Trace;
 class Example {
     File(getParameter("input1-value"));
 
-    File(getParameter("input1-va1l111111111"));
-
+    File(getParameter("input1-va222"));
 
 
 }

6accdf6df830f68647af9b36f4c49362a1045b34
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-01-02 14:35:07 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -4,6 +4,8 @@ class Example {
     File(getParameter("input1-value"));
 
     File(getParameter("input1-va222"));
+    File(getParameter("input1-va22233"));
+    File(getParameter("input1-va222444"));
 
 
 }

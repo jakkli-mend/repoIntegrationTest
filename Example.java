dcc9110618a3d6e0b0945d4d0b1b17e072755206
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-01-03 19:39:44 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -3,9 +3,7 @@ import folder1.folder2.Trace;
 class Example {
     File(getParameter("input1-value"));
 
-    File(getParameter("input1-va222"));
-    File(getParameter("input1-va22233"));
-    File(getParameter("input1-va22266"));
+    File(getParameter("input1-11"));
 
 
 }

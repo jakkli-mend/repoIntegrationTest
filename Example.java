d4e657d6558f44ecf9ef0d8b29b528441c433532
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-01-19 20:54:26 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -3,10 +3,10 @@ class Example {
 
     File(getParameter("input2-value"));
     
-    File(getParameter("aaa66fsg6625588"));
-    File(getParameter("aaa66sdfg88661355"));
-    File(getParameter("aaa66sdfg86862554"));
-    File(getParameter("aaa668sdfg99663555"));
+    File(getParameter("asdf"));
+    File(getParameter("asdwerf"));
+    File(getParameter("erte"));
+    File(getParameter("wert"));
 
 
 }

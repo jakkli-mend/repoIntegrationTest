1981b690508a813f39135a2e0621734cbc6655ce
Jakub Kliwison <jakub.kliwison@mend.io>
2023-01-20 07:53:21 +0100

    Update

--- Example.java
+++ Example.java
@@ -4,9 +4,9 @@ class Example {
     File(getParameter("input2-value"));
     
     File(getParameter("ghd56745456g"));
-    File(getParameter("asgasdxbvcb4567341fdj3456jhjdwerf"));
-    File(getParameter("vbxcvbcvb"));
-    File(getParameter("xcvbxcvb"));
+    File(getParameter("asgasdxbvsdfgcb4567341fdj3456jhjdwerf"));
+    File(getParameter("vbxcvdsfgbcvb"));
+    File(getParameter("xcvbsdfgxcvb"));
 
 
 }

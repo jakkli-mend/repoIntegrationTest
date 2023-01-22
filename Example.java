de4f6e53fa41a408a498c59b55667c1fb55a96c8
Jakub Kliwison <jakub.kliwison@mend.io>
2023-01-22 23:45:47 +0100

    Update

--- Example.java
+++ Example.java
@@ -3,7 +3,7 @@ class Example {
 
     File(getParameter("input2-value"));
     
-    File(getParameter("777"));
-    File(getParameter("888"));
+    File(getParameter("11"));
+    File(getParameter("22"));
 
 }

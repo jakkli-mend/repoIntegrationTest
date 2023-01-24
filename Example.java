ffbd1d31f726e7b5db310580513a6d4d05b8c750
Jakub Kliwison <jakub.kliwison@mend.io>
2023-01-24 18:54:28 +0100

    Update

--- Example.java
+++ Example.java
@@ -2,6 +2,8 @@ class Example {
     File(getParameter("input1-value"));
     File(getParameter("input2-value"));
     File(getParameter("input3-value"));
+    File(getParameter("input1212-value"));
+    File(getParameter("inewerwe-value"));
 
     
 

5f128f31b112c7c36a710f44f108904fc1f75c87
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-01-30 19:22:03 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,21 +1,10 @@
 class Example {
     File(getParameter("input1-value"));
     File(getParameter("input2-value"));
-    
-
-    
-    
     File(getParameter("input3-value"));
     
-    File(getParameter("input1212-value"));
-    
-    File(getParameter("inewerwe-value"));
-    
-    File(getParameter("inewerwe-valuwertwere"));
-     File(getParameter("inewerwe-vasdfgsdfluwertwere"));
-    
-    File(getParameter("inewerwe"));
-
-    
+    File(getParameter("input4-value"));
+    File(getParameter("input5-value"));
+    File(getParameter("input6-value"));
 
 }

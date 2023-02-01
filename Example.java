83aa76164f79675d9cf2bcbe5cedce1ea310d653
Jakub Kliwison <jakub.kliwison@mend.io>
2023-02-01 19:34:56 +0100

    removal

--- Example.java
+++ Example.java
@@ -1,8 +1,5 @@
 class Example {
     File(getParameter("input1-value"));
-    File(getParameter("input2-value"));
-    File(getParameter("5463456345-value"));
-    File(getParameter("5463456345-3456345"));
-    File(getParameter("5463456345-value"));
-    File(getParameter("5463456345-eeeeeeeeeeeee"));
+    File(getParameter("input3333333333-value"));
+
 }

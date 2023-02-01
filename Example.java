738545b5285d2caedc2bf755348cc0f15ccf5c07
Jakub Kliwison <jakub.kliwison@mend.io>
2023-02-01 23:07:28 +0100

    example

--- Example.java
+++ Example.java
@@ -1,12 +1,5 @@
 class Example {
     File(getParameter("input1-value6"));
-    File(getParameter("input1-ertwertw"));
-    File(getParameter("input1-111111111"));
-    File(getParameter("input1-222222222"));
-    File(getParameter("input1-22333333333333333"));
-    File(getParameter("input1-11111111111111111111"));
-    File(getParameter("input1-1"));
-    File(getParameter("input1-2"));
-    File(getParameter("input1-3"));
+
 
 }

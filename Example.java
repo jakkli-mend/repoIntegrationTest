2a5dc17935fd97b4b1c1238d995f6e08a1ac6301
Jakub Kliwison <jakub.kliwison@mend.io>
2023-02-01 16:15:34 +0100

    Example

--- Example.java
+++ Example.java
@@ -3,4 +3,6 @@ class Example {
     File(getParameter("input2-value"));
     File(getParameter("5463456345-value"));
     File(getParameter("5463456345-3456345"));
+    File(getParameter("5463456345-value"));
+    File(getParameter("5463456345-3456345"));
 }

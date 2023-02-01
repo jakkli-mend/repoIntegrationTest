804207bcc013888a3205d02f80e10a80cbbf10db
Jakub Kliwison <jakub.kliwison@mend.io>
2023-02-01 22:56:13 +0100

    example

--- Example.java
+++ Example.java
@@ -6,5 +6,6 @@ class Example {
     File(getParameter("input1-22333333333333333"));
     File(getParameter("input1-11111111111111111111"));
     File(getParameter("input1-1"));
+    File(getParameter("input1-2"));
 
 }

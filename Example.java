a9bd62f14d2563d449e3bf3b01bce42ea667491f
Jakub Kliwison <jakub.kliwison@mend.io>
2023-02-15 09:37:05 +0100

    WS removed

--- Example.java
+++ Example.java
@@ -3,7 +3,7 @@ class Example {
     File(getParameter("input1-bbbbb"));
     File(getParameter("input1-vvvvvvvvvv"));
     File(getParameter("input1-cccccccc"));
-    File(getParameter("input1-wwwwwwwwwwwwwww"));
-    File(getParameter("input1-rrrrrrrrrrrr"));
+    File(getParameter("input1-dfgsdfgsdfgsdfgs"));
+    File(getParameter("input1-fgsdfgsdfgsdfgsd"));
 
 }

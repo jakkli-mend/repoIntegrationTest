e1021d1997f7fbc39fd25928716f700615177d74
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-07-25 16:31:00 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -6,4 +6,7 @@ File(getParameter("input1-aaaaaa23"));
 File(getParameter("input1-aaaaaaa23"));
 File(getParameter("input1-aaaaaaaa23"));
 File(getParameter("input1-aaaaaaaaa23"));
+File(getParameter("input1-aaaaaaa234"));
+File(getParameter("input1-aaaaaaaa234"));
+File(getParameter("input1-aaaaaaaaa234"));
 }

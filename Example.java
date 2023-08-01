229de2311b951b83d0917749206f625891e7a34f
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2023-08-01 19:54:27 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,11 +1,8 @@
 class Example {
     File(getParameter("input1-aaa"));
     File(getParameter("input1-aaaaaaaaa"));
-File(getParameter("input1-aaaaaaaaa111"));
-File(getParameter("input1-aaaaaaaaa111weqweq"));
-File(getParameter("input1-aaaaaaaaa111weqweq1111"));
+File(getParameter("input1-aaaaaaasdfgaa111"));
+File(getParameter("input1-aaaaaasdfgaaa111weqweq"));
+File(getParameter("input1-aaaaaasdfgaaa111weqweq1111"));
 
-File(getParameter("input1-aaaaadaa2aa111"));
-File(getParameter("input1-aaaaaafa3aa111weqweq"));
-File(getParameter("input1-aaaaaagaa4a111weqweq1111"));
 }

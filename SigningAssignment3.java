2eb5a4051237b16f0db20d0da06763f4221c969a
jakkli-mend <117362921+jakkli-mend@users.noreply.github.com>
2024-11-19 18:35:40 +0100

    Update SigningAssignment3.java

--- SigningAssignment3.java
+++ SigningAssignment3.java
@@ -3,8 +3,7 @@
  *
  * Copyright (c) 2002 - 2019 Bruce Mayhew
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * This program is free software; you 
  * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
